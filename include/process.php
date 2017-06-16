b047231f228e4d97314e88adf762d2c146109d01
root <hnjogu@ymail.com>
2017-06-16 15:38:16 +0300

    udated

--- include/process.php
+++ include/process.php
@@ -1,26 +1,23 @@
 <?php
-$name = $_POST["name"];
+
 $email = $_POST["email"];
-$message = $_POST["message"];
+
+// enter the emil to receive data
  
-$EmailTo = "emailaddress@test.com";
-$Subject = "New Message Received";
+$EmailTo = "harristars@gmail.com";
+$Subject = "enquiry  From you guys";
  
 // prepare email body text
-$Body .= "Name: ";
-$Body .= $name;
-$Body .= "\n";
+
  
 $Body .= "Email: ";
 $Body .= $email;
 $Body .= "\n";
  
-$Body .= "Message: ";
-$Body .= $message;
-$Body .= "\n";
  
 // send email
-$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+$success = mail($EmailTo, "From:".$email);
+//$success = mail($EmailTo, $Subject, $Body, "From:".$email);
  
 // redirect to success page
 if ($success){
